3147613a495801046199b29c08d978e1eca970a0
Kono <anupam.nitrkl@gmail.com>
2025-05-12 22:50:46 +0530

    remove duplicates in array, brute force

---
+++ Problems/Arrays/Easy/remove-dupicates.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Brute force->using set to remove duplicates
+//T.C=o(nlogn) S.C=o(n)
+int bruteForce(int arr[], int n) {
+    set<int> s;
+    for (int i = 0; i < n; i++) {
+        s.insert(arr[i]);
+    }
+    int k = s.size();
+    int j = 0;
+    for (int x : s) {
+        arr[j++] = x;
+    }
+    return k;
+}
+
+int main() {
+    int arr[] = {1, 1, 2, 2, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k =bruteForce(arr, n);
+    cout << "The array after removing duplicate elements is: ";
+    for (int i = 0; i < k; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
